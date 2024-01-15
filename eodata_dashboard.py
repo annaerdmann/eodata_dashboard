efcfe47b042c540461bf56ddf2d42785e4ff674b
Anna-Lena <an.le.er@web.de>
2024-01-15 15:17:37 +0100

    eodata dashboard powered by streamlit

---
+++ eodata_dashboard.py
@@ -0,0 +1,30 @@
+# import packages
+
+import streamlit as st
+
+
+
+# Build the app with Streamlit
+
+st.set_page_config(
+    page_title="Overview",
+    page_icon="👋",
+)
+
+st.write("""
+# /eodata 
+#### Analysis of Cloudferros object storage earth observation data archive /eodata
+\n
+\n
+Contents of the Dashboard: 
+
+1. Exploration tool for /eodata Documentation
+
+2. Sentinel 3 data in /eodata
+
+""")
+ 
+# tab1, tab2, tab3 = st.tabs(["1. Data Offer", "2. Completeness", "3. Comparison with EUM"])
+#st.line_chart(ds['availability_duration'])
+
+
